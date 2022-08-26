081110ef5e43d40dd494aec36666d843299c10ee
Alexander Staff <alexandermstaff@gmail.com>
2022-08-26 23:03:21 +0200

    Player input and movement implemented

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,88 @@
+use std::io;
+
+use console::{Key, Term};
+
 const BOARD_SIZE: usize = 16;
+const BLANK_SQUARE: char = ' ';
+
+struct Player {
+    x: usize,
+    y: usize,
+    symbol: char,
+}
+
+impl Player {
+    fn new() -> Player {
+        Player {
+            x: 10,
+            y: 10,
+            symbol: '@',
+        }
+    }
+}
 
 fn main() {
-    let board = build_board();
-    print_board(board);
+    let mut board = build_board();
+    let mut player = Player::new();
+    game_loop(&mut board, &mut player).unwrap();
+    println!("Thanks for playing!")
 }
+
+fn game_loop(board: &mut Vec<Vec<char>>, player: &mut Player) -> io::Result<()> {
+    let term = Term::stdout();
+
+    term.hide_cursor()?;
+    term.clear_screen()?;
+
+    println!("Use arrow keys to controll digger.");
+    println!("Esc ends the game.");
+    println!("Press any key to start");
+
+    term.read_key()?;
+
+    loop {
+        term.clear_screen()?;
+
+        board[player.y][player.x] = player.symbol;
+        print_board(&board);
+
+        let user_move = term.read_key()?;
+        match user_move {
+            Key::ArrowUp => {
+                board[player.y][player.x] = BLANK_SQUARE;
+                player.y -= 1;
+            }
+            Key::ArrowDown => {
+                board[player.y][player.x] = BLANK_SQUARE;
+                player.y += 1;
+            }
+            Key::ArrowLeft => {
+                board[player.y][player.x] = BLANK_SQUARE;
+                player.x -= 1;
+            }
+            Key::ArrowRight => {
+                board[player.y][player.x] = BLANK_SQUARE;
+                player.x += 1;
+            }
+            Key::Escape => {
+                break;
+            }
+            _ => {
+                continue;
+            }
+        }
+    }
+    term.clear_screen()?;
+    term.show_cursor()?;
+
+    Ok(())
+}
+
+// fn move_player()
 
 fn build_board() -> Vec<Vec<char>> {
     let vert_wall = '|';
     let hor_wall = '#';
-    let blank_square = ' ';
 
     let top_and_bottom = vec![hor_wall; BOARD_SIZE + 2];
     let mut board = Vec::with_capacity(BOARD_SIZE + 2);
@@ -19,7 +93,7 @@ fn build_board() -> Vec<Vec<char>> {
         let mut row = Vec::with_capacity(BOARD_SIZE + 2);
         row.push(vert_wall);
         for _ in 0..BOARD_SIZE {
-            row.push(blank_square);
+            row.push(BLANK_SQUARE);
         }
         row.push(vert_wall);
         board.push(row);
@@ -31,7 +105,7 @@ fn build_board() -> Vec<Vec<char>> {
     board
 }
 
-fn print_board(board: Vec<Vec<char>>) {
+fn print_board(board: &Vec<Vec<char>>) {
     for row in 0..BOARD_SIZE + 2 {
         for cell in 0..BOARD_SIZE + 2 {
             print!("{}", board[row][cell])
